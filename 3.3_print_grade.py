e1bd04750b1f89a04308ef80cb58bdc497e533ac
richnathan <richnathan@gmail.com>
2016-07-02 09:38:05 +0800

    Create 3.3_print_grade.py

---
+++ 3.3_print_grade.py
@@ -0,0 +1,21 @@
+score = float(raw_input("Enter Score between 0.0 and 1.0: "))
+
+if score > 1:
+	score = float(raw_input("Enter Score between 0.0 and 1.0 "))
+
+elif score < 0:
+		score = float(raw_input("Enter Score between 0.0 and 1.0 "))
+    
+else:    
+    if score >= 0.9:
+        grade= 'A'
+    elif score >=0.8:
+        grade= 'B'
+    elif score >=0.7:
+        grade= 'C'
+    elif score >=0.6:
+        grade= 'D'
+    else :
+        score >=0.5
+        grade= 'F'
+    print grade
